64b2529c0d3de33c151f03328baa8ee5c060295e
Jim Ianelli <jim.ianelli@gmail.com>
2025-05-03 08:02:16 -0700

    added Matt's stuff

--- R/GetResults.R
+++ R/GetResults.R
@@ -316,7 +316,7 @@ Plot_SSB <- function(df = all_ts) {
 #Plot_SSB(rbind(pm_run$ts) ) 
 
 
-Plot_Rec <- function(df = all_ts) {
+Plot_Rec <- function(df = all_ts, width=0.5,alpha=1,size=.8) {
   p1 <- df |>
     filter(type %in% c("Recruits"), Year < 2025, Year > 1963) |>
     mutate(lnhat = log(value), cv = se/value, 
@@ -324,14 +324,13 @@ Plot_Rec <- function(df = all_ts) {
            lb = exp(lnhat - 1.96 * lnse),
            ub = exp(lnhat + 1.96 * lnse)) |>
     ggplot(aes(x = Year, ymin = lb, ymax = ub, y = value, color = source)) +
-    geom_point(size=.8, position = position_dodge(width = 0.5), stat = "identity") +
-    geom_errorbar(position = position_dodge(width = 0.5), width = 0.4) +
-    geom_line(stat = "identity", position = position_dodge(width = 0.5), linewidth=.1) +
+    geom_point(size=size, position = position_dodge(width = 0.5), stat = "identity") +
+    geom_errorbar(position = position_dodge(width = 0.5), width = .2, linewidth=width, alpha=alpha) +
+    geom_line(stat = "identity", position = position_dodge(width = 0.5), linewidth=.2, alpha=alpha) +
     ggthemes::theme_few() +
     ylab("Age-1 recruits") +
     xlab("Year") +
-    ylim(0, NA) +
-    facet_grid(type ~ ., scales = "free_y")
+    ylim(0, NA) 
   
   return(p1)
 }
